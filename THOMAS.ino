1712d0189f01237c33674789730fe5ed056fe5ae
LukeH71 <lukedhickey@icloud.com>
2025-09-15 14:27:35 -0400

    Update THOMAS.ino
    Fix a multitude of bugs

--- THOMAS.ino
+++ THOMAS.ino
@@ -1,16 +1,16 @@
 /*
       THOMAS 
-      - Temperature & Humidity Observing/Monitoring Assembled Sensor
+      - Temperature & Humidity Observation/Monitoring Assembled Sensor
     Arduino Uno Code
 
   This is the code for a temperature logger that saves the temperature and humidity over time
-  to a microSD card, in a format that is easy to use in graphs through importing to programs
+  to a micrsd card, in a format that is easy to use in graphs through importing to programs
   such as google spreadsheet.
 
-  When you start the program, you will be prompted on the digital display to insert an SD card.
+  When you start the program, you will be prompted on the digital display to insert ansd card.
   Once inserted, and a button is pressed to start, the temperature starts to log. Once finished,
   press the button to access the menue, scroll to the "off" button, and click it. You can then
-  scan that microSD from a computer, find the correct .dat file, and import it into a program
+  scan that micrsd from a computer, find the correct .dat file, and import it into a program
   such as google sheets or exel.
 
   Wiring:
@@ -20,7 +20,7 @@
     GND -> GND
     DATA -> D2
 
-  SD Card Module
+sd Card Module
     VCC -> 5V
     GND -> GND
     CS -> D10
@@ -31,13 +31,13 @@
   RTC (DS3231)
     VCC -> 5V
     GND -> GND
-    SDA -> A4
+  sdA -> A4
     SCL -> A5
 
   LCD1602 I²C
     VCC -> 5V
     GND -> GND
-    SDA -> A4
+  sdA -> A4
     SCL -> A5
 
   Push Button
@@ -54,19 +54,21 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <SPI.h>
-#include <SD.h>
+#include <SdFat.h>
 #include "RTClib.h"
+#include <limits.h>
 
 // Define pins
-const int chipSelect = 10;  // SD card reader chip select wired to D10
+const int chipSelect = 10;  //sd card reader chip select wired to D10
 const int ledPin = 4; // Red LED wired to D4
 const int buttonPin = 7; // Menue / navigator button wired to D7
 #define DHTPIN 2 // Temperature logger data pin wired to D2
 #define DHTTYPE DHT22 // Define sensor type (DHT22, not DHT11)
+#define USE_LONG_FILE_NAMES 1 // For timestamped file names without needing to cram in numbers
 
 // Define modules
 LiquidCrystal_I2C lcd(0x27, 16, 2); // LCD
-DHT dht(DHTPIN, DHTTYPE); // DHT22 / temperature logger
+DHT dht(DHTPIN, DHTTYPE); // DHT22 / temperature reader
 RTC_DS3231 rtc; // RTC / real time clock
 
 // Enums for code readability
@@ -78,17 +80,19 @@ enum Modes {
 };
 
 enum States {
+
   ERROR_RTC_LOST_POWER = 1,
   ERROR_DATAFILE_FAILED = 5,
   ERROR_TIME_TXT_NOT_FOUND = 7,
   ERROR_TIME_TXT_FORMAT_INCORRECT = 8,
 
-  DEFAULT = 2,
+  DEFAULT_STATE = 2,
+  CHECK_RTC = 0,
 
   READY_FOR_BUTTON_INPUT = 3,
   BUTTON_PRESSED = 4,
 
-  DONE_WORKING_MAY_REMOVE_SD = 6
+  DONE_WORKING_MAY_REMOVEsd = 6
 };
 
 // Mode and state variables. 'mode' is to select between 4 different main modes: off,
@@ -100,8 +104,8 @@ enum States {
 // global namespace, and to maintain efficiency. In the idling logic, it is used to manage
 // button presses. In the off screen, it is used as different messages to display. In the
 // menue, it is used to select which mode to enter.
-Modes mode;
-States state = DEFAULT;
+Modes mode = OFF;
+States state = CHECK_RTC;
 
 // Various timers/cooldowns
 unsigned long lastPressTime = 0; // Menue cooldown to actually enter a mode
@@ -110,28 +114,28 @@ unsigned long logCooldown = 0; // Log cooldown to prevent a log every clock cycl
 unsigned long preventDoubleClick = 0; // Used in the menue to prevent electrical false double-clicks
 
 // The reference time, which is used to correctly assign each log with a timestamp
-unsigned int refTime;
+unsigned long refTime;
 
 // Different durations / frequencies
 #define IDOLING_DURATION 20000
 #define REMAIN_IDOL_DURATION 30000
 #define LOG_FREQUENCY 5000
-#define MENUE_SELECT_DURATION 1000
+#define MENUE_SELECT_DURATION 2000
 #define COUNTER_FAKE_BUTTON_RELEASE_DURATION 50
 
 
 // The statistics viewable in the latter 2 modes
 unsigned long numBytes = 0;
-unsigned int timeSeconds = 0;
+unsigned long timeSeconds = 0;
 float humidity = 0; 
 float temperature = 0; 
 
 // A counter used to preserve data incase of unplugging, but also displayed occationally
 unsigned long numDataPoints = 0;
 
-// The file in which logs are stored
+// The file in which logs are stored, and thesd from which is being read
 File dataFile;
-
+SdFat sd;
 
 
 
@@ -148,16 +152,16 @@ void setup() {
   lcd.backlight();  // Turn on the backlight
 
   if (!rtc.begin()) {
-    Serial.println("Couldn't find RTC");
+    Serial.println(F("Couldn't find RTC"));
     while (1);
   }
 
-  if (rtc.lostPower()) {
-    Serial.println("RTC lost power, setting time!");
+  if (!sd.begin(chipSelect, SPI_FULL_SPEED)) {
+      state = ERROR_DATAFILE_FAILED;
 
-    logCooldown = millis(); // In this instance, logCooldown is used to temprorarially display an error message
-    state = ERROR_RTC_LOST_POWER;
-  }
+      Serial.println(F("failed"));
+      return;
+    }
 
   // Get the reference time for timestamping
   DateTime reference = rtc.now();
@@ -175,14 +179,13 @@ void setup() {
 // Set up 'dataFile' with the correct name. This is called in mode == 0 / OFF
 void defineDateFile(){
 
-
-  if(rtc.lostPower() && !SD.exists("time.txt")) { // If the rtc lost power, and there is no replacement, no name can be generated
+  if(rtc.lostPower() && !sd.exists(F("time.txt"))) { // If the rtc lost power, and there is no replacement, no name can be generated
     state = ERROR_TIME_TXT_NOT_FOUND;
     return;
 
   } else if(rtc.lostPower()) { // If the rtc lost power, but there is a possible replacement, try to find the name
 
-    timeFile = SD.open("time.txt", FILE_READ);
+    File timeFile =sd.open(F("time.txt"), FILE_READ);
 
     // Save the contents of 'timeFile' into 'savedName'
     char savedName[64];
@@ -215,10 +218,11 @@ void defineDateFile(){
     // 6 - MM_DD_YY_HH_mm
     // 7 - MM_DD_YY_HH_mm_SS
     //
-    
-
 
     for (int i = 0; i<len; ++i){
+
+      if(savedName[i]=='\n' || (uint8_t)savedName[i] == 13) { continue; }
+
       if(isAlpha(savedName[i]) && savedName[i] != '_'){ // Call an error if a character is neither a letter, nor an '_'
         state = ERROR_TIME_TXT_FORMAT_INCORRECT;
         return;
@@ -228,9 +232,11 @@ void defineDateFile(){
         if (savedName[i] == '_') { // Specifically underscores:
           concurrentUnderlines += 1;
 
+          concurrentNumbers = 0;
           if (i==0){ // Time.txt cannot start with '_'
             state = ERROR_TIME_TXT_FORMAT_INCORRECT;
             return;
+
           }
 
           if(concurrentUnderlines == 2) { // A double underscore can only uccor on the 2 formats where an X is supported
@@ -240,10 +246,12 @@ void defineDateFile(){
               format = 5;
             } else {
               state = ERROR_TIME_TXT_FORMAT_INCORRECT;
+
               return;
             }
           } else if (concurrentUnderlines > 2){ // A triple+ underscore is never supported
             state = ERROR_TIME_TXT_FORMAT_INCORRECT;
+
             return;
           }
 
@@ -283,8 +291,11 @@ void defineDateFile(){
     // MM DD YY HH mm SS XX -> YY MM DD HH mm SS
     rtc.adjust(DateTime((atoi(date[2])+2000), atoi(date[0]), atoi(date[1]), atoi(date[3]), atoi(date[4]), atoi(date[5])));
 
+    DateTime reference = rtc.now();
+    refTime = reference.unixtime();
+
     // If the file name exists, and there is not a number ontop of it already, append a __X
-    if (SD.exists(savedName)&&format!=2&&format!=4){
+    if (sd.exists(savedName)&&format!=2&&format!=4){
       int i = 0;
 
       // Loop until a number that does not exist is found
@@ -294,24 +305,27 @@ void defineDateFile(){
 
         snprintf(fileName, sizeof(fileName), "%s__%i.dat", savedName, i);
 
-        if(!SD.exists(fileName)){
+        if(sd.exists(fileName)){
           strcpy(savedName, fileName);
           break;
         }
 
       }
 
-    } else if (SD.exists(savedName)) { // If the file name exists, but there is already an __X suffix, throw an error, because the user needs more/less detail
+    } else if (sd.exists(savedName)) { // If the file name exists, but there is already an __X suffix, throw an error, because the user needs more/less detail
       state = ERROR_TIME_TXT_FORMAT_INCORRECT;
+
       return;
 
     } else { // If the file does not exist, create the file name
       snprintf(savedName, sizeof(savedName), "%s.dat", savedName);
     }
 
+    Serial.println(savedName);
+
     // Open the file name :)
-    dataFile = SD.open(savedName, FILE_WRITE);
-    SD.remove("time.txt");
+    dataFile =sd.open(savedName, FILE_WRITE);
+    sd.remove("time.txt");
 
   } else { // If the RTC remained to have power, create the file based off of that in format 6 (MM_DD_YY_HH_mm)
     char fileName[64];
@@ -319,7 +333,9 @@ void defineDateFile(){
     snprintf(fileName, sizeof(fileName), "%02d_%02d_%02d_%02d_%02d.dat", rtc.now().month(), rtc.now().day(), (rtc.now().year()-2000), rtc.now().hour(), rtc.now().minute());
 
     // This level of accuracy should not already have a file attatched to it, so it can confidentally be assigned a value
-    dataFile = SD.open(fileName, FILE_WRITE);
+    dataFile =sd.open(fileName, FILE_WRITE);
+
+    Serial.println(fileName);
   }
 }
 
@@ -336,7 +352,7 @@ void loop() {
 
   // Logic for logging the temp/humidity as long as the mode != 0 / off
   // + logic for idling the screen to prevent overheating
-  if (mode != 0) {
+  if (mode != OFF) {
 
     // Logging logic
     if (millis() - logCooldown > LOG_FREQUENCY) {
@@ -351,44 +367,44 @@ void loop() {
     }
 
     // Screen idling logic (as long as mode != 1 / mode select)
-    if (mode != 1) { 
+    if (mode != MODE_SELECT) { 
       screenIdle();
     }
   }
 
   // Handle each mode individually in a switch statement
   switch (mode) {
-    case 0: // The logic of inputing or taking out SD 
+    case OFF: // The logic of inputing or taking outsd 
       offMode();
       break;
-    case 1: // Menue to change what to display, or to turn off
+    case MODE_SELECT: // Menue to change what to display, or to turn off
       menue();
       break;
-    case 2: // Temp / humidity
-      if (millis() - idolingTime > IDOLING_DURATION) { // Don't print if screen is idle
+    case TEMP_HUMIDITY_VIEW: // Temp / humidity
+      if (millis() - idolingTime < IDOLING_DURATION) { // Don't print if screen is idle
         lcd.setCursor(0,0);
-        lcd.print("Temp: ");
+        lcd.print(F("Temp: "));
         lcd.print(temperature);
-        lcd.print(" degC ");
+        lcd.print(F(" degC "));
         lcd.setCursor(0,1);
-        lcd.print("Humid: ");
+        lcd.print(F("Humid: "));
         lcd.print(humidity);
-        lcd.print(" %RH ");
+        lcd.print(F(" %RH "));
       }
       break;
-    case 3: // Duration / file size
-      if (millis() - idolingTime > IDOLING_DURATION) { 
+    case DURATION_BYTES_VIEW: // Duration / file size
+      if (millis() - idolingTime < IDOLING_DURATION) { 
         lcd.setCursor(0,0);
         lcd.print(timeSeconds);
-        lcd.print(" Sec elps        ");
+        lcd.print(F(" Sec elps        "));
         lcd.setCursor(0,1);
         if(numDataPoints%2==0) { // Toggle between the number of data points and the number of bytes
-          lcd.print("~");
+          //lcd.print(F();
           lcd.print(numBytes);
-          lcd.print(" Bytes            ");
+          lcd.print(F(" Bytes            "));
         } else {
           lcd.print(numDataPoints);
-          lcd.print(" Data pts            ");
+          lcd.print(F(" Data pts            "));
         }
       }
       break;
@@ -408,14 +424,14 @@ void log() {
 
   // Check if any reads have failed
   if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Failed to read from DHT22 sensor!");
+    Serial.println(F("Failed to read from DHT22 sensor!"));
     return;
   }
  
   numBytes += dataFile.print(timeSeconds);
-  numBytes += dataFile.print(","); 
+  numBytes += dataFile.print(F(",")); 
   numBytes += dataFile.print(temperature);
-  numBytes += dataFile.print(",");
+  numBytes += dataFile.print(F(","));
   numBytes += dataFile.print(humidity);
   numBytes += dataFile.println();
 }
@@ -440,12 +456,19 @@ void screenIdle(){
   // state == 4       After releasing the button to wake the screen, meaning that
   // the user pressed the button, and released it.
 
+  if (millis() - idolingTime < IDOLING_DURATION && state == 2) {
+    state = 4;
+  }
+
   int buttonState = digitalRead(buttonPin);
   if (buttonState == LOW && (state == 2 || state == 4)) {
 
-    if (millis() - idolingTime < IDOLING_DURATION && state == 4) {
+    if (millis() - idolingTime < IDOLING_DURATION && state == 4 && mode != MODE_SELECT) {
       mode = MODE_SELECT;
+      state = 0;
       lastPressTime = millis();
+
+      return;
 
     } else {
       idolingTime = millis();
@@ -465,73 +488,93 @@ void screenIdle(){
 
 
 // Mode 0 - off mode 
-// Inserting SD, formatting SD, and removing SD
+// Insertingsd, formattingsd, and removingsd
 void offMode(){
 
   int buttonState = digitalRead(buttonPin);
 
+  // Check wether the RTC lost power in loop-time
+  if (state==CHECK_RTC) {
+    if(rtc.lostPower()){
+      Serial.println(F("RTC lost power, setting time!"));
+
+      logCooldown = millis(); // In this instance, logCooldown is used to temprorarially display an error message
+      state = ERROR_RTC_LOST_POWER;
+    } else {
+      state = DEFAULT;
+    }
+  }
+  
+
+
   // Handle different cases 
   switch(state) {
     case ERROR_RTC_LOST_POWER:
-      lcd.setCursor(0, 0);
-      lcd.print("RTC lost power,");
-      lcd.setCursor(0, 1);
-      lcd.print("input time 2 SD");
-      if (millis() - logCooldown < 7000){
-        state = 3;
+      
+      if (millis() - logCooldown > 7000){
+        state = READY_FOR_BUTTON_INPUT;
+
+        lcd.setCursor(0, 0);
+        lcd.print(F("Once done,      "));
+        lcd.setCursor(0, 1);
+        lcd.print(F("press button    "));
+      } else {
+        lcd.setCursor(0, 0);
+        lcd.print(F("RTC lost power, "));
+        lcd.setCursor(0, 1);
+        lcd.print(F("input time to SD"));
       }
       break;
     case ERROR_DATAFILE_FAILED:
       lcd.setCursor(0, 0);
-      lcd.print("Error starting,    ");
+      lcd.print(F("Error starting, "));
       lcd.setCursor(0, 1);
-      lcd.print("please retry :)    ");
-      state = 3;
+      lcd.print(F("please retry :) "));
+      state = READY_FOR_BUTTON_INPUT;
       break;
     case ERROR_TIME_TXT_NOT_FOUND:
       lcd.setCursor(0, 0);
-      lcd.print("Error: time.txt");
+      lcd.print(F("Error: time.txt "));
       lcd.setCursor(0, 1);
-      lcd.print("file not found.");
-      state = 3;
+      lcd.print(F("file not found  "));
+      state = READY_FOR_BUTTON_INPUT;
       break;
     case ERROR_TIME_TXT_FORMAT_INCORRECT:
       lcd.setCursor(0, 0);
-      lcd.print("Error: time.txt");
+      lcd.print(F("Error: time.txt "));
       lcd.setCursor(0, 1);
-      lcd.print("formatted wrong");
-      state = 3;
+      lcd.print(F("formatted wrong "));
+      state = READY_FOR_BUTTON_INPUT;
       break;
-    case DEFAULT:
+    case DEFAULT_STATE:
       lcd.setCursor(0, 0);
-      lcd.print("Please insert  ");
+      lcd.print(F("Please insert SD"));
       lcd.setCursor(0, 1);
-      lcd.print("SD & press ->  ");
-      state = 3;
+      lcd.print(F("and press button"));
+      state = READY_FOR_BUTTON_INPUT;
       break;
-    case DONE_WORKING_MAY_REMOVE_SD:
+    case DONE_WORKING_MAY_REMOVEsd:
       lcd.setCursor(0, 0);
-      lcd.print("You may remove ");
+      lcd.print(F("You may remove  "));
       lcd.setCursor(0, 1);
-      lcd.print("SD or restart  ");
-      state = 3;
+      lcd.print(F("SD or restart :D"));
+      state = READY_FOR_BUTTON_INPUT;
       break;
   }
 
   // Handle RTC lost power error case as well, because the warning is just a temporary popup, and can be ignored
   if (buttonState == LOW && (state==READY_FOR_BUTTON_INPUT || state==ERROR_RTC_LOST_POWER)) {
+
     state = BUTTON_PRESSED;
 
   } else if (buttonState == HIGH && state==BUTTON_PRESSED) {
 
-    if (!SD.begin(chipSelect)) {
-      state = ERROR_DATAFILE_FAILED;
-      return;
-    }
+    lcd.clear();
 
     // Formatting correctly requires complexity, and such is packaged into a function.
     defineDateFile(); // Define the data file through the RTC, and time.txt if applicable
     // skip the following code if an error is thrown in the above function to prevent from crashing / undifined behavior
+
     if (state == ERROR_TIME_TXT_FORMAT_INCORRECT || state == ERROR_TIME_TXT_NOT_FOUND ) { return; }
     
     if (!dataFile) {
@@ -539,13 +582,14 @@ void offMode(){
 
     } else {
 
-      numBytes += dataFile.println("Time (Seconds),Temperature (Celcius), Humidity (%RH)");
+      numBytes += dataFile.println(F("Time (Seconds),Temperature (Celcius), Humidity (%RH)"));
 
-      state = DEFAULT;
+      state = DEFAULT_STATE;
 
       idolingTime = millis();
       mode = TEMP_HUMIDITY_VIEW;
       digitalWrite(ledPin, LOW); // LED is only active when not recording
+
     }
   }
 }
@@ -569,27 +613,28 @@ void offMode(){
 
 void menue(){
 
+
   // Go to the mode if button has not been pressed in one second
   if (millis() - lastPressTime > MENUE_SELECT_DURATION && state%2==0) {
 
     switch(state){
       case 2:
         mode = TEMP_HUMIDITY_VIEW;
-        state = DEFAULT;
+        state = DEFAULT_STATE;
 
         idolingTime = millis();
 
         break;
       case 4:
-        mode = DURITION_BYTES_VIEW;
-        state = DEFAULT;
+        mode = DURATION_BYTES_VIEW;
+        state = DEFAULT_STATE;
 
         idolingTime = millis();
 
         break;
       case 6:
         mode = OFF;
-        state = DONE_WORKING_MAY_REMOVE_SD;
+        state = DONE_WORKING_MAY_REMOVEsd;
 
         dataFile.close();
         digitalWrite(ledPin, HIGH);
@@ -605,11 +650,11 @@ void menue(){
   int buttonState = digitalRead(buttonPin);
   if (buttonState == LOW && state%2==0) {
 
-    state += 1;
+    state = (States)((int)state + (int)1);
 
     // Loop back to the first element if already seen all previous elements
     if(state>5){
-      state -= 6;
+      state = (States)((int)state - (int)6);
     }
     
     // Type an '→' character next to the current selection
@@ -617,44 +662,41 @@ void menue(){
       case 1:
         lcd.clear();
         lcd.setCursor(0,0);
-        lcd.print(" ");
+        lcd.print(F(" "));
         lcd.write(byte(126));
-        lcd.print("1     2     3");
+        lcd.print(F("1     2     3"));
         
         break;
       
       case 3:
         lcd.clear();
         lcd.setCursor(0,0);
-        lcd.print("  1    ");
+        lcd.print(F("  1    "));
         lcd.write(byte(126));
-        lcd.print("2     3");
+        lcd.print(F("2     3"));
         
         break;
 
       case 5:
         lcd.clear();
         lcd.setCursor(0,0);
-        lcd.print("  1     2    ");
+        lcd.print(F("  1     2    "));
         lcd.write(byte(126));
-        lcd.print("3");
+        lcd.print(F("3"));
         
         break;
     }
 
     // The choices between menues
     lcd.setCursor(0,1);
-    lcd.print("temp  info  off");
+    lcd.print(F("temp  info  off"));
 
   } else if(buttonState == HIGH && state%2==1) { // Handle fake double clicks
-    state += 1;
-    preventDoubleClick = millis() + COUNTER_FAKE_BUTTON_RELEASE_DURATION;
-  } else if (buttonState == HIGH && state%2==0 && preventDoubleClick - millis() > 0){
+    state = (States)((int)state + (int)1);
+    preventDoubleClick = millis();
+  } else if (buttonState == HIGH && state%2==0 && preventDoubleClick != 0 && millis() - preventDoubleClick > COUNTER_FAKE_BUTTON_RELEASE_DURATION){
     lastPressTime = millis();
     preventDoubleClick = 0;
+
   }
 }
-
-
-
-
